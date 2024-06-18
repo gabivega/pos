23685b3336242e74e73140feab7569a811aaccbb
Gabi <gabi.vega1988@gmail.com>
2024-06-17 21:11:20 -0300

    add render url

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
         email: email,
         password: password,
       };
-      const response = await fetch("http://localhost:3001/login", {
+      const response = await fetch("https://pos-y99w.onrender.com/login", {
         method: "POST",
         headers: {
           "content-type": "application/json",
